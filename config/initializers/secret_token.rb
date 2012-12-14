1f1ac3ffba99474cf31b800786973bbbaac3faee
vagrant <vagrant@lucid32.hsd1.ca.comcast.net>
2012-12-14 17:11:46 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '48057b900c34367cf2d8667afdc7d4a1881c660ec6ff66656eab5998e79167b2e43f7a6221b17cd8e156d4d82e8fd7bd1d552bdf8bcad557447fcc8c9a1b9f73'
